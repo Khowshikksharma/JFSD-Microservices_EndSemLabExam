9b68f2b784b17a9ca414ab2aba27c61e46a41384
S Khowshikk Sharma <2200031954@kluniversity.in>
2024-12-07 11:04:55 +0530

    V1.0

--- src/main/java/com/jfsd/exam/controller/CustomerController.java
+++ src/main/java/com/jfsd/exam/controller/CustomerController.java
@@ -2,11 +2,13 @@ package com.jfsd.exam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.jfsd.exam.models.Customer;
 import com.jfsd.exam.service.CustomerService;
@@ -17,6 +19,12 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @GetMapping("/")
+    public ModelAndView getupdatepage() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("updatecustomer");
+    	return mv;
+    }
     @PostMapping
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerService.saveCustomer(customer);
