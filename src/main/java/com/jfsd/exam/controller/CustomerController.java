6166296e7da73247389c44dd6a5d9f347fee9ab0
S Khowshikk Sharma <2200031954@kluniversity.in>
2024-12-07 11:10:09 +0530

    V1.0

--- src/main/java/com/jfsd/exam/controller/CustomerController.java
+++ src/main/java/com/jfsd/exam/controller/CustomerController.java
@@ -1,20 +1,19 @@
 package com.jfsd.exam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jfsd.exam.models.Customer;
 import com.jfsd.exam.service.CustomerService;
 
-@RestController
-@RequestMapping("/api/customers")
+@Controller
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
